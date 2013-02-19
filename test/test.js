b6b58b4e1f4c47f018b984570d35d43c5ca05dbd
Daniel Quirino Oliveira <daniel.quirino@moip.com.br>
2013-02-19 20:05:10 -0300

    credit card date expiration check - ok

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 load("../lib/moip.js");
 
-var valid = moip.creditCard.isValid("4111111111111111");
-print("Valid? "+valid);
-var cscValid = moip.creditCard.isSecurityCodeValid("12345");
-print("CSC Valid? "+cscValid);
-var type = moip.creditCard.cardType("5105105105105100");
-print("Type "+type["brand"]);
-var isExpiryDateValid = moip.creditCard.isExpiryValid("10", "2013");
+
+var isExpiryDateValid = moip.creditCard.isExpiryDateValid("10", "2013");
+print("ExpiryDateValid? "+isExpiryDateValid);
+var isExpiryDateValid = moip.creditCard.isExpiryDateValid("02", "2013");
+print("ExpiryDateValid? "+isExpiryDateValid);
+var isExpiryDateValid = moip.creditCard.isExpiryDateValid("10", "2010");
 print("ExpiryDateValid? "+isExpiryDateValid);
