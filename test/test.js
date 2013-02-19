42cf19782967ae7a9c94bb8af35d113a598d173b
Daniel Quirino Oliveira <daniel.quirino@moip.com.br>
2013-02-18 21:58:33 -0300

    First commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+load("../lib/moip.js");
+
+var valid = moip.creditCard.isValid("12345");
+print("Valid? "+valid);
+var cscValid = moip.creditCard.isSecurityCodeValid("12345");
+print("CSC Valid? "+cscValid);
+var type = moip.creditCard.cardType("12345");
+print("Type? "+type["brand"]);
+var isExpiryDateValid = moip.creditCard.isExpiryValid("10", "2013");
+print("ExpiryDateValid? "+isExpiryDateValid);
