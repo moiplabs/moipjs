22ad7dfb68ca498e4397b651410b8412773943e6
Breno Oliveira <breno.oliveira@moip.com.br>
2013-03-08 15:54:56 -0300

    roda o qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,15 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    qunit: {
+      all: ['test/**/*.html']
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit')
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['qunit','uglify']);
 
 };
