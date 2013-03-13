efdb66e7715413439376af9fd0d073a9ba5be135
Breno Oliveira <breno.oliveira@moip.com.br>
2013-03-13 13:45:09 -0300

    adiciona a data de build no header do javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,10 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    banner: "Moip by Moip",
-    // concat: {
-    //   options: {
-    //     banner: '<%= banner %>',
-    //     stripBanners: true
-    //   },
-    //   dist: {
-    //     src: ['src/<%= pkg.name %>.js'],
-    //     dest: 'build/<%= pkg.name %>.js'
-    //   },
-    // },
-
+    banner: "Moip.js",
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= banner %> - build date: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: 'src/<%= pkg.name %>.js',
