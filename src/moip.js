ffcb334a55f7f20d77322f8b3a616e78418643a8
Danillo Souza <danillo.fs@gmail.com>
2013-04-23 10:21:35 -0300

    adicionando calculo com juros

--- src/moip.js
+++ src/moip.js
@@ -1,18 +1,18 @@
 var moip = {
 	creditCard: {
-	 	isValid: function(creditCardNumber) {
+		isValid: function(creditCardNumber) {
 			creditCardNumber = creditCardNumber.replace(/\s+/g, '');
 			creditCardNumber = creditCardNumber.replace(/\./g, '');
 			creditCardNumber = creditCardNumber.replace(/\-/g, '');
 			// Luhn algorithm: http://en.wikipedia.org/wiki/Luhn_algorithm
 			var checksum = 0;
-		  for (var i=(2-(creditCardNumber.length % 2)); i<=creditCardNumber.length; i+=2) {
-		     checksum += parseInt(creditCardNumber.charAt(i-1));
-		  }
+			for (var i=(2-(creditCardNumber.length % 2)); i<=creditCardNumber.length; i+=2) {
+				checksum += parseInt(creditCardNumber.charAt(i-1));
+			}
 		  // Analyze odd digits in even length strings or even digits in odd length strings.
 		  for (var i=(creditCardNumber.length % 2) + 1; i<creditCardNumber.length; i+=2) {
-		     var digit = parseInt(creditCardNumber.charAt(i-1)) * 2;
-		     if (digit < 10) { checksum += digit; } else { checksum += (digit-9); }
+		  	var digit = parseInt(creditCardNumber.charAt(i-1)) * 2;
+		  	if (digit < 10) { checksum += digit; } else { checksum += (digit-9); }
 		  }
 		  if ((checksum % 10) == 0) return true; else return false;
 
@@ -23,10 +23,10 @@ var moip = {
 			creditCardNumber = creditCardNumber.replace('.', '');
 			creditCardNumber = creditCardNumber.replace('-', '');
 			var regexpVisa = /^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/;
-      var regexpMaster = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;
-      var regexpAmex = /^3[4,7]\d{13}$/;
-      var regexpDiners = /^3[0,6,8]\d{12}$/;
-      var regexHiper = /^(606282\d{10}(\d{3})?|3841\d{15})$/;
+			var regexpMaster = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;
+			var regexpAmex = /^3[4,7]\d{13}$/;
+			var regexpDiners = /^3[0,6,8]\d{12}$/;
+			var regexHiper = /^(606282\d{10}(\d{3})?|3841\d{15})$/;
 			
 			if(creditCardNumber.match(regexpVisa))
 				return {"brand":"VISA"};
@@ -73,12 +73,8 @@ var moip = {
 
 	calculator: {
 
-		creditCardPricing: function(json){
+		pricing: function(json){
 			return this.buildJson(json);
-		},
-
-		otherPricing: function(){
-
 		},
 
 		buildJson: function(json){
@@ -92,6 +88,7 @@ var moip = {
 			
 			if (json.antecipation_percentage != undefined && json.floating != undefined && json.installment != undefined) {
 				installmentValue = this._calculateInstallmentValue(json.amount, json.installment);
+
 				for(i = 0; i <= 11; i++){
 					antecipationPercentageArr[i] = this._calculateAntecipationPercentage(transaction_tax, json, i);
 					totalTaxArr[i] = this._calculateTotalTax(antecipationPercentageArr[i], transaction_tax);
@@ -100,7 +97,30 @@ var moip = {
 				return { "amount" : json.amount/100, "transaction_tax" : transaction_tax, "antecipation_percentage" : antecipationPercentageArr, "total_tax" :  totalTaxArr, "liquid_value" : liquidValueArr, "installment" : installmentValue};
 			} else {
 				liquidValueArr = this._calculateLiquidValue(json.amount, transaction_tax);
+				return { "amount" : json.amount/100, "transaction_tax" : transaction_tax, "liquid_value" : liquidValueArr };
 			};
+		},
+
+		pricingWithInterest: function(json){
+			var interestRate = new Array();
+			var amount = new Array();
+			var transaction_tax;
+			var installmentValue;
+			var antecipationPercentageArr = new Array();
+			var totalTaxArr = new Array();
+			var liquidValueArr = new Array();
+
+			transaction_tax = this._calculateTransactionTax(json.amount, json.transaction_percentage, json.fixed);
+			installmentValue = this._calculateInstallmentValue(json.amount, json.installment);
+
+			for (var i = 1; i <= 12; i++) {
+				interestRate[i - 1] = this._calculateInterestRate(json.amount, json.interest_rate, i);
+				amount[i - 1] = this._calculateAmount(interestRate[i - 1], i);
+				antecipationPercentageArr[i - 1] = this._calculateAntecipationPercentage(transaction_tax, json, i - 1);
+				totalTaxArr[i -1] = this._calculateTotalTax(antecipationPercentageArr[i -1], transaction_tax);
+				liquidValueArr[i -1] = this._calculateLiquidValue(json.amount, totalTaxArr[i -1]);
+			}
+			return { "amount" : amount, "transaction_tax" : transaction_tax, "antecipation_percentage" : antecipationPercentageArr, "total_tax" :  totalTaxArr, "liquid_value" : liquidValueArr, "installment" : installmentValue};
 		},
 
 		_calculateTransactionTax: function(amount, transactionPercentage, fixed){
@@ -116,11 +136,26 @@ var moip = {
 		},
 
 		_calculateLiquidValue: function(transactionValue, totalTax){
-			return parseFloat(transactionValue)/100 + parseFloat(totalTax);
+			return parseFloat(((transactionValue)/100 + parseFloat(totalTax)).toFixed(2));
 		},
 
 		_calculateInstallmentValue: function(amount, installment){
 			return parseFloat(((amount / installment) / 100).toFixed(2));
+		},
+
+		_calculateInterestRate: function(amount, interestRate, installment){
+			if (installment == 1){
+				return parseFloat(amount/100);
+			}
+			return parseFloat(this._coefficient(interestRate, installment) * (amount/100));
+		},
+
+		_calculateAmount: function(interestRate, installment){
+			return parseFloat((interestRate * installment).toFixed(2));
+		},
+
+		_coefficient: function(interestRate, installment){
+			return parseFloat(((interestRate / 100)/(1-1/(Math.pow(((interestRate / 100)+1), installment)))).toFixed(10));
 		}
 	}
 };
